e0006d22e3d10d06d8d843e3f629af22b8c43e72
William <willvsouza@gmail.com>
2022-10-21 20:13:12 -0300

    inicio do projeto

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once('../vendor/autoload.php');
+
+use src\controller\TesteController;
+
+$controller = new TesteController();
---
+++ src/controller/TesteController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace src\controller;
+
+class TesteController{
+    public function __construct()
+    {
+        echo 'estamos aqui'; 
+    }
+}
